19a35c99b18c665f3e5484a22eb232c02bd411c5
Igor Nasteski <igor.nasteski92@gmail.com>
2020-12-24 02:40:05 +0100

    upgrading restTemplate communication with external online API (api.themoviedb.org) to get movie title and overview

--- src/main/java/igor/n/movieinfoservice/controller/MovieController.java
+++ src/main/java/igor/n/movieinfoservice/controller/MovieController.java
@@ -1,15 +1,14 @@
 package igor.n.movieinfoservice.controller;
 
 import igor.n.movieinfoservice.model.Movie;
+import igor.n.movieinfoservice.model.MovieSummary;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
@@ -18,30 +17,20 @@ public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${api.key}")
+    private String apiKey;
+
     @GetMapping("/{movieId}")
     public Movie getMovies(@PathVariable("movieId")String movieId){
-        Movie movie = new Movie(movieId, "nekiFilmIzMovie-info-service");
-        movies.add(movie1);
-        movies.add(movie2);
-        movies.add(movie3);
-        movies.add(movie4);
-        movies.add(movie5);
 
-        for(int i=0; i<movies.size(); i++){
-            if(movies.get(i).getMovieId().equalsIgnoreCase(movieId)){
-                return movies.get(i);
-            }
-        }
+        //zovem spoljni free api: na sajtu "https://api.themoviedb.org" sam se registrovao i onda dobio "api_key" bez kog ne daju da
+        //se posalje restTemplate request ka njima(smestio sam ga u application.properties fajl  i ovde ga pozivam)
+        //takodje, morao sam da napravim novu klasu "MovieSummary", da bih u njoj smestio one podatke koji mi trebaju(id,title,overview)
+        //pa onda te podatke iskoristim da setujem objekat "Movie" i da ga onda vratim na api "movie-catalog-service" jer odatle gadjam ovaj endpoint
+        MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId +
+                                                                            "?api_key=" + apiKey, MovieSummary.class);
 
-        return null;
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
     }
-
-    Movie movie1 = new Movie("1", "Lord Of The Rings");
-    Movie movie2 = new Movie("2", "Harry Potter 1");
-    Movie movie3 = new Movie("3", "Harry Potter 2");
-    Movie movie4 = new Movie("4", "Harry Potter 3");
-    Movie movie5 = new Movie("5", "Harry Potter 4");
-
-    List<Movie> movies = new ArrayList<>();
 
 }
